d2ce52ef51abdc86283b4bb3f8cc0fe12d7266cf
David Yuan <davidyuan@utexas.edu>
2017-03-18 17:46:31 -0500

    done

---
+++ PhotoPoster/PhotoCell.swift
@@ -0,0 +1,24 @@
+//
+//  PhotoCell.swift
+//  PhotoPoster
+//
+//  Created by David Yuan on 3/17/17.
+//  Copyright © 2017 David Yuan. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ PhotoPoster/Post.swift
@@ -0,0 +1,13 @@
+//
+//  Post.swift
+//  PhotoPoster
+//
+//  Created by David Yuan on 3/17/17.
+//  Copyright © 2017 David Yuan. All rights reserved.
+//
+
+import UIKit
+
+class Post: NSObject {
+
+}
